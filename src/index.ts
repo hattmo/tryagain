f610dd1953e456a8994b352deab377d1d73dce1b
Matthew Howard <matthew@hattmo.com>
2019-07-03 17:00:04 -0500

    first draft

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-export default function(): boolean {
-  process.stdout.write("Hattmo Template Library Not Yet Implemented\n");
-  return true;
+export default async function <T>(times: number,
+                                  body: () => Promise<T> | T,
+                                  errorHandler?: (error: any, count: number) => any): Promise<T> {
+  let count = 1;
+  while (count < times) {
+    try {
+      return await body();
+    } catch (error) {
+      if (errorHandler) {
+        errorHandler(error, count);
+      }
+    }
+    count++;
+  }
+  return await body();
 }
--- test/mainTest.ts
+++ test/mainTest.ts
@@ -1,8 +1,20 @@
 // tslint:disable: no-unused-expression
 import { expect } from "chai";
-import index from "../src/index";
+import tryagain from "../src/index";
 describe("Tests", () => {
-    it("Should have tests", () => {
-        expect(index()).to.be.true;
+    it("Should run the function 5 times if the function always throws", async () => {
+        try {
+            await tryagain(5, () => {
+                add(2, 3);
+            });
+            expect.fail();
+        } catch (err) {
+            expect(err).to.exist;
+        }
     });
 });
+
+function add(a: number, b: number): number {
+    //   throw new Error("Errored");
+    return a + b;
+}
