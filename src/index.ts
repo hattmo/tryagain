d2602b63fcfc614502719e2eca76338c96030289
Hattmo <matthew@hattmo.com>
2019-07-03 23:26:32 -0500

    added sinon and updated tests

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 export default async function <T>(times: number,
                                   body: (attempt?: number) => Promise<T> | T,
-                                  errorHandler?: (error: any, attempt?: number) => any): Promise<T> {
+                                  errorHandler?: (error: any, attempt: number, exit: () => void) => any): Promise<T> {
   let count = 1;
-  while (count < times) {
+  while (count < times || times === 0) {
     try {
       return await body(count);
-    } catch (error) {
+    } catch (iterationError) {
+      let exitCalled = false;
       if (errorHandler) {
-        errorHandler(error, count);
+        await errorHandler(iterationError, count,
+          () => {
+            exitCalled = true;
+          });
+      }
+      if (exitCalled) {
+        throw iterationError;
       }
     }
     count++;
--- test/mainTest.ts
+++ test/mainTest.ts
@@ -1,20 +1,58 @@
 // tslint:disable: no-unused-expression
 import { expect } from "chai";
 import tryagain from "../src/index";
+import sinon from "sinon";
+
 describe("Tests", () => {
     it("Should run the function 5 times if the function always throws", async () => {
+        const fakeFunction = sinon.fake.throws(new Error("Error Thrown"));
         try {
             await tryagain(5, () => {
-                add(2, 3);
+                fakeFunction();
+            });
+            expect.fail("should not complete");
+        } catch (error) {
+            expect(fakeFunction.callCount).to.be.equal(5);
+        }
+    });
+    it("should run the function once if the function returns", async () => {
+        const fakeFunction = sinon.fake.returns(true);
+        try {
+            await tryagain(5, () => {
+                fakeFunction();
+            });
+        } catch (error) {
+            expect.fail("Should not throw and error");
+            return;
+        }
+        expect(fakeFunction.calledOnce).to.true;
+    });
+    it("should run until stop is called when the first argument is 0", async () => {
+        const fakeFunction = sinon.fake.throws(new Error("Error Thrown"));
+        try {
+            await tryagain(0, () => {
+                fakeFunction();
+            }, (_error, attempt, exit) => {
+                if (attempt === 7) {
+                    exit();
+                }
+            });
+        } catch (error) {
+            expect(fakeFunction.callCount).to.equal(7);
+            return;
+        }
+        expect.fail();
+    });
+    it("should work with an async function rejecting", async () => {
+        const fakeFunction = sinon.fake.rejects(new Error("Error Thrown"));
+        try {
+            await tryagain(4, async () => {
+                await fakeFunction();
+                return true;
             });
             expect.fail();
-        } catch (err) {
-            expect(err).to.exist;
+        } catch (error) {
+            expect(fakeFunction.callCount).to.equal(4);
         }
     });
 });
-
-function add(a: number, b: number): number {
-    //   throw new Error("Errored");
-    return a + b;
-}
